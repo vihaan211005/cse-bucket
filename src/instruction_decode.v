7276841efc98b1681dc4db900b25c015ffa9e3bf
Vihaan Sapra <vihaansapra@gmail.com>
2025-04-15 23:37:36 +0530

    Fix decode

--- src/instruction_decode.v
+++ src/instruction_decode.v
@@ -1,17 +1,20 @@
 module instruction_decode (
     input  [31:0] instruction,
     output [ 5:0] opcode,
+    output [25:0] adr,
     output [ 4:0] rs,
     output [ 4:0] rt,
     output [ 4:0] rd,
+    output [ 4:0] shamt,
+    output [ 6:0] funct,
     output [15:0] imm
 );
-  assign opcode = instr[31:26];  // R, I, J
-  assign adr = instr[25:0];  // J
-  assign rs = instr[25:21];  // R, I
-  assign rt = instr[20:16];  // R, I
-  assign rd = instr[15:11];  // R
-  assign shamt = instr[11:7];  // R
-  assign funct = instr[6:0];  // R
-  assign imm = instr[15:0];  // I
+  assign opcode = instruction[31:26];  // R, I, J
+  assign adr = instruction[25:0];  // J
+  assign rs = instruction[25:21];  // R, I
+  assign rt = instruction[20:16];  // R, I
+  assign rd = instruction[15:11];  // R
+  assign shamt = instruction[11:7];  // R
+  assign funct = instruction[6:0];  // R
+  assign imm = instruction[15:0];  // I
 endmodule
